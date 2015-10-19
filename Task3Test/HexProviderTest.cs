81d8c67d4002dbc6e1d20647f922df410a0e147b
stas <stanislav.berezovsky.95@gmail.com>
2015-10-19 23:11:00 +0300

    Add Task3 and Task3Test

---
+++ Task3Test/HexProviderTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task3;
+
+namespace Task3Test
+{
+    [TestClass]
+    public class HexProviderTest
+    {
+        [TestMethod]
+        public void FormatIntToHexTest()
+        {
+            HexProvider hexProvider = new HexProvider();
+            Assert.AreEqual(string.Format(hexProvider, "{0:H}", -231), "-E7");
+            Assert.AreEqual(string.Format(hexProvider, "{0:H}", 10045), "273D");
+            Assert.AreEqual(string.Format(hexProvider, "{0:H}", -234456), "-393D8");
+        }
+    }
+}
