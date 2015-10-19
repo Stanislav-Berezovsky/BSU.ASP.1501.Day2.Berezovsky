a79e11299ac5cb484dc102030885c2162d5c5c6c
stas <stanislav.berezovsky.95@gmail.com>
2015-10-19 16:07:53 +0300

    add cs modul

---
+++ Task1Test/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task1;
+
+namespace Task1.Test
+{
+    [TestClass]
+    public class NewtonMethodTest
+    {
+
+
+        [TestMethod]
+        public void CompareWithMathPowTest()
+        {
+            Assert.AreEqual(NewtonMethod.FindingTheRoot(64, 3, 0.0001), Math.Pow(64, 1.0 / 3), 0.0001);
+        }
+
+        [TestMethod]
+        public void NumberIsNanTest()
+        {
+            Assert.AreEqual(NewtonMethod.FindingTheRoot(-17, 4, 0.0001), double.NaN);
+        }
+
+        [TestMethod]
+        public void ExponentIsOneTest()
+        {
+            Assert.AreEqual(NewtonMethod.FindingTheRoot(121, 0, 0.0001), 1);
+        }
+
+        [TestMethod]
+        public void NumberIsZero()
+        {
+            Assert.AreEqual(NewtonMethod.FindingTheRoot(0, 7, 0.0001), 0);
+        }
+    }
+}
+
