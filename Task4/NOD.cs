534f2e589dc8db976063c376476003a59b909849
stas <stanislav.berezovsky.95@gmail.com>
2015-10-20 07:12:17 +0300

    Add Task4 And Task4est

---
+++ Task4/NOD.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Task4
+{
+    public static class NOD
+    {
+        public static int EuclideanMethod(int a ,int b)
+        {
+            a=Math.Abs(a);
+            b=Math.Abs(b);
+            int variable = 0;
+            while (b > 0)
+            {
+                variable = b;
+                b = a % b;
+                a = variable;
+            }
+            return a;
+        }
+
+        public static int EuclideanMethod(int a, int b, out long ticks)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int result = EuclideanMethod(a, b);
+            stopwatch.Stop();
+            ticks = stopwatch.ElapsedTicks;
+            return result;
+        }
+
+        public static int EuclideanMethod(params int[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+            if (array.Length == 0)
+                throw new ArgumentException();
+            int result = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                result = EuclideanMethod(result, array[i]);
+            }
+            return result;  
+        }
+
+
+        public static long EuclideanMethod(out long ticks, params int[] array)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int result = EuclideanMethod(array);
+            stopwatch.Stop();
+            ticks = stopwatch.ElapsedTicks;
+            return result;
+        }
+
+        public static int SteinMethod(int a, int b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            if (a == 0)
+                return b;
+            if (b == 0)
+                return a;
+            if (a == b)
+                return a;
+            if (a == 1 && b==1)
+                return 1;
+            if (a % 2 == 0)
+            {
+                if (b % 2 == 0)                   
+                    return 2*SteinMethod(a/2, b/2) ;
+                return SteinMethod(a/2, b);
+            }
+            if (b % 2 == 0)
+                return SteinMethod(a, b/2);
+
+            if (a > b)
+                return SteinMethod((a - b)/2, b);
+            return SteinMethod((b - a) /2, a);
+        }
+
+
+        public static int SteinMethod(int a, int b, out long ticks)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int result = SteinMethod(a, b);
+            stopwatch.Stop();
+            ticks = stopwatch.ElapsedTicks;
+            return result;
+        }
+
+
+        public static int SteinMethod(params int[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+            if (array.Length == 0)
+                throw new ArgumentException();
+            int result = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                result = SteinMethod(result, array[i]);
+            }
+            return result;
+        }
+
+
+        public static long SteinMethod(out long ticks, params int[] array)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int result = SteinMethod(array);
+            stopwatch.Stop();
+            ticks = stopwatch.ElapsedTicks;
+            return result;
+        }
+    }
+}
---
+++ Task4Test/NODTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task4;
+using System.Diagnostics;
+
+namespace Task4Test
+{
+    [TestClass]
+    public class NODTest
+    {
+        [TestMethod]
+        public void EuclideanMethodTest()
+        {
+            long ticks = 0;
+            Assert.AreEqual(NOD.EuclideanMethod(1000,-750), 250);
+            Assert.AreEqual(NOD.EuclideanMethod(out ticks,1000, -750), 250);
+            Debug.WriteLine("EuclideanMethodTime  :{0}", ticks);
+            Assert.AreEqual(NOD.EuclideanMethod(1000, -750,800,550), 50);
+            Assert.AreEqual(NOD.EuclideanMethod(out ticks, 1000, -750, 800, 550), 50);
+            Debug.WriteLine("EuclideanMethodArrayTime  :{0}", ticks);
+        }
+
+        [TestMethod]
+        public void SteinMethodTest()
+        {
+            long ticks = 0;
+            Assert.AreEqual(NOD.SteinMethod(1000,-750),250);
+            Assert.AreEqual(NOD.SteinMethod(out ticks, 1000, -750), 250);
+            Debug.WriteLine("EuclideanMethodTime  :{0}", ticks);
+            Assert.AreEqual(NOD.SteinMethod(1000, -750, 800, 550), 50);
+            Assert.AreEqual(NOD.SteinMethod(out ticks, 1000, -750, 800, 550), 50);
+            Debug.WriteLine("EuclideanMethodArrayTime  :{0}", ticks);
+        }
+
+    }
+    
+    
+}
